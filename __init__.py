c9dffb97005a63c576c44459e8ca218d45dde46c
Daniel <124807187+slavndn@users.noreply.github.com>
2025-01-09 23:52:30 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,8 @@
 import sys
 import os
-# Добавляем путь к папке Users
 sys.path.append(os.path.join(os.path.dirname(__file__), 'app'))
 
-# Добавляем путь к папке App
+
 sys.path.append(os.path.join(os.path.dirname(__file__), 'bonus'))
 
 sys.path.append(os.path.join(os.path.dirname(__file__), 'data_base'))
